8a877be8d362c9bfdae6a58027f7293ec6f26b6e
Dirli <d@dirli.at>
2022-08-04 20:22:01 +0200

    Remove autocommands

--- nvim/lua/d/autocommands.lua
+++ nvim/lua/d/autocommands.lua
@@ -13,18 +13,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
   end
 })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  group = general_group,
-  command = "set nobuflisted"
-})
-
-local resize_group = vim.api.nvim_create_augroup("_Resize", {})
-vim.api.nvim_create_autocmd("VimResized", {
-  pattern = "*",
-  group = resize_group,
-  command = "tabdo wincmd = "
-})
 
 local tabs_and_spaces_group = vim.api.nvim_create_augroup("_TabsAndSpaces", {})
 vim.api.nvim_create_autocmd("FileType", {
@@ -35,5 +23,5 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "java",
   group = tabs_and_spaces_group,
-  command = "set autoindent noexpandtab tabstop=4 shiftwidth=4 formatoptions+=cro"
+  command = "set autoindent noexpandtab tabstop=4 shiftwidth=4"
 })
