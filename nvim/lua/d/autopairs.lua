56f9274d782c1912323feebd7807e38f947f337a
Dirli <d@dirli.at>
2022-08-04 20:31:20 +0200

    Improve autopairs

--- nvim/lua/d/autopairs.lua
+++ nvim/lua/d/autopairs.lua
@@ -1,4 +1,3 @@
--- Setup nvim-cmp.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
   return
@@ -10,17 +9,8 @@ npairs.setup {
     lua = { "string", "source" },
     javascript = { "string", "template_string" },
   },
-  disable_filetype = { "TelescopePrompt", "spectre_panel" },
   fast_wrap = {
     map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0, -- Offset from pattern match
-    end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
-    check_comma = true,
-    highlight = "PmenuSel",
-    highlight_grey = "LineNr",
   },
 }
 
