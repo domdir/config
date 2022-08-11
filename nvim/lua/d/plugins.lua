56f13d4cb6a4729083bbc4777087b5699a619853
Dirli <d@dirli.at>
2022-08-11 17:18:04 +0200

    Add telescope dap

--- nvim/lua/d/plugins.lua
+++ nvim/lua/d/plugins.lua
@@ -105,6 +105,7 @@ return packer.startup(function(use)
   }
   use {"nvim-telescope/telescope-fzf-native.nvim", run = "make" }
   use "nvim-telescope/telescope-ui-select.nvim"
+  use "nvim-telescope/telescope-dap.nvim"
 
   -- Treesitter
   use {
